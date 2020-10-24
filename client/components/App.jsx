741d553c37df02f3ae2b4d6593b9c817fef31624
Duy Nguyen <bobcat.2604@gmail.com>
2020-10-24 11:29:13 -0400

    test hooking up with algorithm, close to success

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import AddTodo from './AddTodo';
 import TodoList from './TodoList';
 
-import { useFilamentQuery } from '../hooks';
+import { useFilamentQuery, mergeDataFromCacheAndServer } from '../hooks';
+import { parseFilamentQuery } from '../hooks/utils';
+import axios from 'axios';
 
 const query = `
 {
@@ -16,7 +18,17 @@ const query = `
 `;
 
 const query2 = `
-{
+query {
+  todos {
+    id
+    text
+    number
+  }
+}
+`;
+
+const query3 = `
+query {
   todos {
     id
     number
@@ -24,28 +36,35 @@ const query2 = `
 }
 `;
 
-/**
- * What to store? What's the key?
- * How to store?
- * How to parse query to get final query?
- * How to combine new data with cache?
- */
+const cache = {
+  todos: [
+    {
+      id: '1',
+      text: 'Build GraphQL server',
+      isCompleted: false,
+      number: 1,
+    },
+    {
+      id: '2',
+      text: 'Build Frontend App',
+      isCompleted: true,
+      number: 12,
+    },
+    {
+      id: '3',
+      text: 'Develop caching system',
+      isCompleted: false,
+      number: 15,
+    },
+  ],
+};
 
-// const mutation = (text) => `
-// mutation {
-//   addTodo(input: "${text}") {
-//     id
-//     text
-//     isCompleted
-//   }
-// }
-// `;
+sessionStorage.clear();
 
 const App = () => {
   const { state, makeQuery } = useFilamentQuery(query, []);
-
-  const addTodo = (text) => {};
-  // useFilamentMutation(mutation(text), (result) => setTodos(result));
+  const [isOpen, setIsOpen] = useState(false);
+  const [dataFromDB, setDataFromDB] = useState('');
 
   const toggleTodo = (id) => {
     const newTodos = todos.map((todo) =>
@@ -60,15 +79,50 @@ const App = () => {
     setTodos(newTodos);
   };
 
+  const handleClick = () => {
+    const [finalQuery, dataInCache] = parseFilamentQuery(query2);
+    setIsOpen(!isOpen);
+    axios.post('/graphql', { query: finalQuery }).then((res) => {
+      setDataFromDB(res.data.data.todos);
+    });
+  };
+
+  const displayCode = (cacheString) => {
+    return JSON.stringify(JSON.parse(cacheString), null, 2);
+  };
+
   return (
     <div className="App">
-      <h1>Todo App</h1>
+      <div style={{ display: 'flex' }}>
+        <label>
+          Make a Query
+          <textarea cols="30" rows="10" value={query2}></textarea>
+        </label>
+        <br />
+
+        <label>
+          Updated Query
+          <textarea cols="30" rows="10" value={isOpen ? query3 : ''}></textarea>
+        </label>
+      </div>
+
+      <div>
+        <span>Data in cache</span>
+        <pre>{displayCode(sessionStorage.getItem('todos'))}</pre>
+      </div>
+      <div>
+        <p>Data fetched:</p>
+        <pre>{displayCode(JSON.stringify(dataFromDB))}</pre>
+      </div>
+      <button onClick={handleClick}>Fetch</button>
+
+      {/* <h1>Todo App</h1>
       <button onClick={() => sessionStorage.clear()}>
         Clear sessionStorage
       </button>
       <button onClick={() => makeQuery(query2)}>Fetch numbers</button>
       <AddTodo addTodo={addTodo} />
-      <TodoList todos={state.todos || state} toggleTodo={toggleTodo} />
+      <TodoList todos={state.todos || state} toggleTodo={toggleTodo} /> */}
     </div>
   );
 };

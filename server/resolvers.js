9d38259814afcb63f663f5e8f77321197e919bac
Andrew <andrew.lovato@gmail.com>
2020-11-11 17:28:42 -0500

    merging changes

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-const Todo = require('./todoModel');
-=======
 const axios = require("axios");
 const Todo = require("./todoModel");
 const Member = require("./memberModel");
->>>>>>> main
 
 const resolvers = {
   Query: {
@@ -30,52 +26,25 @@ const resolvers = {
         const addedTodo = await Todo.create(newTodo);
         return addedTodo;
       } catch (err) {
-<<<<<<< HEAD
         return err
-=======
-        console.log(err);
->>>>>>> main
       }
     },
     async updateTodo(_, { input }) {
       try {
         const { id, text } = input;
-<<<<<<< HEAD
         const updatedTodo = await Todo.findByIdAndUpdate(id, { text }, { new: true })
         return updatedTodo
       } catch (err) {
         return err
-=======
-        // console.log(input)
-        const updatedTodo = await Todo.findByIdAndUpdate(
-          id,
-          { text },
-          { new: true }
-        );
-        return updatedTodo;
-        // return axios
-        //   .post('http://localhost:4000/todos', updatedTodo)
-        //   .then((res) => console.log(res.data))
-      } catch (err) {
-        console.log(err);
->>>>>>> main
       }
     },
     async deleteTodo(_, { input }) {
       try {
         const { id } = input;
-<<<<<<< HEAD
         const deletedTodo = await Todo.findByIdAndDelete(id)
         return deletedTodo
       } catch (err) {
         return err
-=======
-        console.log("ID:", id);
-        const deletedTodo = await Todo.findByIdAndDelete(id);
-        return deletedTodo;
-      } catch (err) {
-        console.log(err);
->>>>>>> main
       }
     },
   },

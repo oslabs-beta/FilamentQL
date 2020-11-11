6887f9df2c96c6dfa59b5d61d3c17045d59c6399
Chan Choi <chanychoi93@gmail.com>
2020-11-11 12:07:13 -0500

    added styling to cards, reordered alignment,need to add pictures

--- client/components/Team/index.jsx
+++ client/components/Team/index.jsx
@@ -1,55 +1,154 @@
-import React from 'react'
+import React from "react";
+import "./Team.scss";
 
 const Team = () => {
   return (
     <div>
-      <div className="Header" style={{
-        display: 'flex',
-        flexDirection: 'column',
-        textAlign: 'center'
-      }}>
+      <div
+        className='Header'
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          textAlign: "center",
+        }}
+      >
         <h1>Meet the Filament Team</h1>
       </div>
-      <div className="container">
-        <div className="card-wrapper">
-          <div className="card">
-            <div className="card-image">
-              <img src="https://image.ibb.co/dUTfmJ/profile_img.jpg" alt="profile one" />
+      <div className='container'>
+        <div className='card-wrapper'>
+          <div className='card'>
+            <div className='card-image'>
+              <img
+                src='https://image.ibb.co/dUTfmJ/profile_img.jpg'
+                alt='profile one'
+              />
             </div>
-            <ul className="social-icons">
+            <ul className='social-icons'>
               <li>
-                <a href="">
-                  <i className="fab fa-facebook-f"></i>
+                <a href='https://github.com/andrew-lovato'>
+                  <i className='fab fa-github'></i>
                 </a>
               </li>
               <li>
-                <a href="">
-                  <i className="fab fa-instagram"></i>
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  <i className="fab fa-twitter"></i>
-                </a>
-              </li>
-              <li>
-                <a href="">
-                  <i className="fab fa-dribbble"></i>
+                <a href='https://www.linkedin.com/in/andrew-lovato/'>
+                  <i className='fab fa-linkedin'></i>
                 </a>
               </li>
             </ul>
-
-            <div className="details">
-              <h2>John Smith
+            <div className='details'>
+              <h2>
+                Andrew Lovato
                 <br />
-                <span className="job-title">UI Developer</span>
+                <span className='job-title'>Software Engineer</span>
               </h2>
             </div>
           </div>
         </div>
+        <div className='bio'>
+          <p>A passionate software developer</p>
+        </div>
+
+        <div className='card-wrapper'>
+          <div className='card'>
+            <div className='card-image'>
+              <img
+                src='https://image.ibb.co/dUTfmJ/profile_img.jpg'
+                alt='profile one'
+              />
+            </div>
+            <ul className='social-icons'>
+              <li>
+                <a href='https://github.com/neljson'>
+                  <i className='fab fa-github'></i>
+                </a>
+              </li>
+              <li>
+                <a href='https://www.linkedin.com/in/nelson-wu-a731781b0'>
+                  <i className='fab fa-linkedin'></i>
+                </a>
+              </li>
+            </ul>
+            <div className='details'>
+              <h2>
+                Nelson Wu
+                <br />
+                <span className='job-title'>Software Engineer</span>
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className='bio'>
+          <p>A passionate software developer</p>
+        </div>
+
+        <div className='card-wrapper'>
+          <div className='card'>
+            <div className='card-image'>
+              <img
+                src='https://image.ibb.co/dUTfmJ/profile_img.jpg'
+                alt='profile one'
+              />
+            </div>
+            <ul className='social-icons'>
+              <li>
+                <a href='https://github.com/bobdeei'>
+                  <i className='fab fa-github'></i>
+                </a>
+              </li>
+              <li>
+                <a href='https://www.linkedin.com/in/duy-nguyen-991351a3/'>
+                  <i className='fab fa-linkedin'></i>
+                </a>
+              </li>
+            </ul>
+            <div className='details'>
+              <h2>
+                Duy Nguyen
+                <br />
+                <span className='job-title'>Software Engineer</span>
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className='bio'>
+          <p>A passionate software developer</p>
+        </div>
+
+        <div className='card-wrapper'>
+          <div className='card'>
+            <div className='card-image'>
+              <img
+                src='https://image.ibb.co/dUTfmJ/profile_img.jpg'
+                alt='profile one'
+              />
+            </div>
+            <ul className='social-icons'>
+              <li>
+                <a href='https://github.com/chanychoi93'>
+                  <i className='fab fa-github'></i>
+                </a>
+              </li>
+              <li>
+                <a href='https://www.linkedin.com/in/chan-choi'>
+                  <i className='fab fa-linkedin'></i>
+                </a>
+              </li>
+            </ul>
+            <div className='details'>
+              <h2>
+                Chan Choi
+                <br />
+                <span className='job-title'>Software Engineer</span>
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className='bio'>
+          <p>A passionate software developer</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Team;

63b365ff24662093f13429776df24959a16b8fde
Chan Choi <chanychoi93@gmail.com>
2020-11-12 08:45:15 -0500

    added fading and delay transition to each page

--- client/components/Team/index.jsx
+++ client/components/Team/index.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import "./Team.scss";
-import Member from "./Member";
-import { getMembersQuery } from "../../query";
+import './Team.scss';
+import Member from './Member';
+import { getMembersQuery } from '../../query';
 
 const Team = () => {
   const [members, setMembers] = useState([]);
 
   useEffect(() => {
-    axios.post("/graphql", { query: getMembersQuery }).then((response) => {
+    axios.post('/graphql', { query: getMembersQuery }).then((response) => {
       const { members } = response.data.data;
       setMembers(members);
     });
@@ -20,13 +20,12 @@ const Team = () => {
       <div
         className='Header'
         style={{
-          display: "flex",
-          flexDirection: "column",
-          textAlign: "center",
+          display: 'flex',
+          flexDirection: 'column',
+          textAlign: 'center',
         }}
       >
         <h1>Meet the FilamentQL Team</h1>
-        <h4>Hover over each image for more information.</h4>
       </div>
       <div className='container'>
         {members.map((member) => (
